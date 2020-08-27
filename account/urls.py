745f4381ea7c01f42ca4b3a38b0bd8b53b09e49a
Anton Chasnyk <anton.chasnyk@gmail.com>
2020-08-27 14:09:49 +0300

    reset password done

--- account/urls.py
+++ account/urls.py
@@ -9,8 +9,6 @@ urlpatterns = [
     path('login', views.log_in, name='login'),
     path('logout', views.log_out, name='logout'),
 
-    # path('password_change', auth_views.PasswordChangeView.as_view(), name='password_change'),
-    # path('password_change/done', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
     path('password_reset', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('password_reset/done', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
