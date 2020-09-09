bad05d88fb482151c7a9547f254e577383e9c71a
Anton Chasnyk <anton.chasnyk@gmail.com>
2020-09-09 11:06:58 +0300

    selactable_search script in file. Can be used several widgets on one page

--- static/assets/js/selectable_search.js
+++ static/assets/js/selectable_search.js
@@ -1,7 +1,15 @@
 "use strict";
 
-function get_searchable_id(hid, inp, option_list, search_url) {
-    let g = $(inp).val();
+let selectors = $("[href][name*='selector']:input");
+for (let i=0; i<selectors.length; i++)
+{
+    let inp = selectors[i];
+    let selector_id = inp.id.split("_");
+    let hid = $("#"+selector_id[0]+"_"+selector_id[1])[0];
+    let option_list = inp.list;
+    let search_url = $(inp).attr('href');
+    $(inp).on( 'input', function() {
+      let g = $(inp).val();
         $.ajax(search_url,{
         type: "GET",
         data: {
@@ -10,13 +18,14 @@ function get_searchable_id(hid, inp, option_list, search_url) {
         },
         success: function (data, textStatus, jqXHR) {
             $(option_list).html(data);
-        },
-        dataType: 'html'
+            },
+            dataType: 'html'
+            });
+            let option_id = $(option_list).attr("id");
+            let option = $('#'+option_id+' option[value="' + g +'"]')[0];
+            if (option)
+                $(hid).val(option.id);
+            else
+                $(hid).val(null);
     });
-    let option_id = $(option_list).attr("id");
-    let option = $('#'+option_id+' option[value="' + g +'"]')[0];
-    if (option)
-        $(hid).val(option.id);
-    else
-        $(hid).val(null);
 }
