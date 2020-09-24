f875a41b008c6312c099130559b256613e108967
Anton Chasnyk <anton.chasnyk@gmail.com>
2020-09-24 16:02:11 +0300

    bom export filename and time

--- static/assets/js/page/datatables.js
+++ static/assets/js/page/datatables.js
@@ -44,6 +44,23 @@ $('#save-stage').DataTable({
 		"scrollX": true,
 		stateSave: true
 	});
+
+function formatDate(date, format) {
+    const map = {
+        mm: ('0' + (date.getMonth() + 1)).slice(-2),
+        dd: ('0' + date.getDate()).slice(-2),
+        yy: date.getFullYear().toString().slice(-2),
+        yyyy: date.getFullYear(),
+        MM: ('0' + date.getMinutes()).slice(-2),
+        HH: ('0' + date.getHours()).slice(-2),
+    };
+    return format.replace(/mm|dd|yy|yyy|HH|MM/gi, matched => map[matched])
+}
+
+function get_bom_title() {
+                    return 'BOM '+ document.title.split(':')[1] + ' ' + formatDate(new Date(), 'dd.mm.yyyy_HH.MM');
+                }
+
 $('#tableExport').DataTable({
 		dom: 'Bfrtip',
         paging: false,
@@ -52,6 +69,7 @@ $('#tableExport').DataTable({
 		buttons: [
             {
                 extend: 'pdf',
+                title: get_bom_title(),
                 exportOptions: {
                     columns: ':visible:not(.notexport)',
                     rows: ':visible:not(.notexport)',
@@ -60,6 +78,7 @@ $('#tableExport').DataTable({
             },
             {
                 extend: 'excel',
+                title: get_bom_title(),
                 exportOptions: {
                     columns: ':visible:not(.notexport)',
                     rows: ':visible:not(.notexport)'
@@ -67,6 +86,7 @@ $('#tableExport').DataTable({
             },
             {
                 extend: 'csv',
+                title: get_bom_title(),
                 exportOptions: {
                     columns: ':visible:not(.notexport)',
                     rows: ':visible:not(.notexport)'
@@ -74,6 +94,7 @@ $('#tableExport').DataTable({
             },
             {
                 extend: 'print',
+                title: get_bom_title(),
                 exportOptions: {
                     columns: ':visible:not(.notexport)',
                     rows: ':visible:not(.notexport)'
